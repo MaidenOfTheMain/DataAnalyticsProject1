002a11ba1bd9990259a6e8974fbd3d661bfd6c29
MaidenoftheMain <marcus.arnett10@gmail.com>
2019-11-20 14:45:43 -0500

    Current working code, need progress on NN interval detection

--- getPeaks.m
+++ getPeaks.m
@@ -1,6 +1,4 @@
-file = load("data.txt");
-t = 0:1:size(file, 1)-1;
-data1 = abs(file);
+data1 = abs(load("data.txt"));
 data1_mean = ceil(mean(data1));
 current_outlier = outlier(data1);
 [out] = rmoutlier(data1);
@@ -9,15 +7,14 @@ while (current_outlier > data1_mean)
   [out] = rmoutlier(out);
   current_outlier = outlier(out);
 endwhile
-[pks idx] = findpeaks(out);
 t_out = 0:1:size(out, 1)-1;
-[yh, lambda] = regdatasmooth (t_out, out, "d",4,"stdev",1e-1,"midpointrule");
+out_smooth = sgolayfilt(sgolayfilt(out));
+[pks idx] = findpeaks(out_smooth, "MinPeakDistance", round(4));
 
-dt = t_out(2)-t_out(1);
-[pks2 idx2] = findpeaks(out, "MinPeakDistance", round(4/dt));
-annotation('textbox',[0.5 0.01 0.1 0.1],'string','my text','fitboxtotext','on');
+
 
 subplot(1, 1, 1)
-plot(t_out,out,t_out(idx2),out(idx2),'or')
+plot(t_out,out_smooth,t_out(idx),out_smooth(idx),'xr')
+title("PPG Data")
 axis tight
 legend("Location","NorthOutside","Orientation","horizontal")
