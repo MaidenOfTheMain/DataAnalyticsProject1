fd7467c8022757cf7f4e705563627ba509e0795f
MaidenoftheMain <marcus.arnett10@gmail.com>
2019-11-21 21:29:22 -0500

    Implement SDNN solver

--- getPeaks.m
+++ getPeaks.m
@@ -10,6 +10,7 @@ endwhile
 t_out = 0:1:size(out, 1)-1;
 out_smooth = sgolayfilt(sgolayfilt(out));
 [pks idx] = findpeaks(out_smooth, "MinPeakDistance", round(4));
+
 NNI = []
 k = 2;
 l = 1;
@@ -20,12 +21,16 @@ for i = 1:length(idx)
     break;
   end
   interval = idx(k) - idx(i);
-  NNI(l) = interval;
+  NNI(l) = interval * pulse_time_interval;
   ++l;
   ++k;
 end
 
+AVNN = mean(NNI);
 
+SDNN = sum(arrayfun(@(k) (NNI(k) - AVNN)^2, 1:length(NNI) ));
+SDNN = SDNN / (length(NNI) - 1);
+SDNN = sqrt(SDNN);
 
 subplot(1, 1, 1)
 plot(t_out,out_smooth,t_out(idx),out_smooth(idx),'xr')
