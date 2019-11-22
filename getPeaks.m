da429a2a9a55947bf031f059d3de1ee44dc5847b
MaidenoftheMain <marcus.arnett10@gmail.com>
2019-11-21 22:03:17 -0500

    Implement AVNN / SDNN display on graph

--- getPeaks.m
+++ getPeaks.m
@@ -3,13 +3,15 @@ data1_mean = ceil(mean(data1));
 current_outlier = outlier(data1);
 [out] = rmoutlier(data1);
 current_outlier = outlier(out);
+
 while (current_outlier > data1_mean)
   [out] = rmoutlier(out);
   current_outlier = outlier(out);
 endwhile
+
 t_out = 0:1:size(out, 1)-1;
 out_smooth = sgolayfilt(sgolayfilt(out));
-[pks idx] = findpeaks(out_smooth, "MinPeakDistance", round(4));
+[pks idx] = findpeaks(out_smooth, "MinPeakDistance", round(2));
 
 NNI = []
 k = 2;
@@ -33,7 +35,8 @@ SDNN = SDNN / (length(NNI) - 1);
 SDNN = sqrt(SDNN);
 
 subplot(1, 1, 1)
-plot(t_out,out_smooth,t_out(idx),out_smooth(idx),'xr')
-title("PPG Data")
+plot(t_out,out_smooth,t_out(idx),out_smooth(idx),'xr;NN Interval;')
+xlabel(['SDNN = ', num2str(SDNN), ' seconds', '                         AVNN = ', num2str(AVNN), ' seconds'])
+title("                                                                                         PPG Data") 
 axis tight
 legend("Location","NorthOutside","Orientation","horizontal")
